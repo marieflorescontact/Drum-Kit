1c501e1f13df179502bffbbf092e48c268cd7ba4
marie flores <marieflores.contact@gmail.com>
2021-03-28 14:19:13 +0200

    JS CSS et HTML ok pour Desktop

---
+++ script.js
@@ -0,0 +1,19 @@
+function playSound(e) {
+  const keyPressed = e.keyCode;
+  const audio = document.querySelector(`audio[data-key='${keyPressed}']`);
+  const key = document.querySelector(`.key[data-key="${keyPressed}"]`);
+  const image = document.querySelector(`img[data-key='${keyPressed}']`);
+  if (!audio) return; //ne joue pas si il n'y pas de son
+  audio.currentTime = 0; //remet le son au debut
+  audio.play();
+  key.classList.add("playing");
+  image.style.visibility("visible");
+}
+//partie faite sans trop comprendre:
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+window.addEventListener("keydown", playSound);
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
